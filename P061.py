43b7b87676d8ccd679c50b4544fe9117419e5dc6
Lester B <tkhdlstfl.l+git@gmail.com>
2012-12-02 10:23:47 +0800

    Determine the sum of cyclic pol numbers (P061)
    Find the sum of six polynomial numbers each of which is a triangle,
    square, pentagonal, hexagonal, heptagonal and octagonal numbers.
    
    The solution finds all the polynomial numbers within the range and put it
    in a dictonary. The dictionary has the number of sides as a key and another
    dictonary for the number generated by the side (triangular, square, etc.). It
    is implemented as dictonary so that the head and the tail of the number can be
    access easily.
    
    Using recursion makes the implementation more understandable because it is the
    nature of the problem.
    
    Because of the way traverse is implemented, duplicate sequence are also
    generated.

---
+++ P061.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+from math import sqrt, ceil, floor
+from itertools import groupby
+
+
+def polygonal_number(s, n):
+    return (n * (n + 1) + (s - 3) * n * (n - 1)) / 2
+
+
+def inverse_polygonal_number(s, N):
+    s = s - 2
+    return ((s - 2) + sqrt(s * s + (8 * N - 4) * s + 4)) / (2 * s)
+
+
+def traverse(head, graph, sides, res):
+    if not sides:
+        yield res
+
+    for i, s in enumerate(sides):
+        subgraph = graph[s]
+        sides_list = list(sides)
+        sides_list.remove(s)
+        try:
+            tails = subgraph[head]
+        except KeyError:
+            continue
+
+        for tail in tails:
+            num = head * 100 + tail
+            if num in res:
+                continue
+            for ret in traverse(tail, graph, sides_list, res + (num,)):
+                yield ret
+
+
+inv = inverse_polygonal_number
+sides = tuple(range(3, 9))
+bounds = ((ceil(inv(i, 1000)), floor(inv(i, 9999))) for i in sides)
+pol_nums = tuple(
+        tuple(int(polygonal_number(s, n)) for n in range(lower, upper + 1))
+            for s, (lower, upper) in zip(sides, bounds))
+pol_nums_split = tuple(
+        tuple((num // 100, num % 100) for num in seq)
+            for seq in pol_nums)
+graph_pol = {}
+for i, seq in zip(sides, pol_nums_split):
+    subgraph = {}
+    graph_pol[i] = subgraph
+    for init, final in seq:
+        subgraph.setdefault(init, [])
+        subgraph[init].append(final)
+
+solution_sets = set()
+for head in graph_pol[sides[0]].keys():
+    ret = traverse(head, graph_pol, sides, ())
+    for candidates in ret:
+        if candidates[-1] % 100 == head:
+            solution_sets.add(frozenset(candidates))
+
+for sol in solution_sets:
+    print(sum(sol), sol)
